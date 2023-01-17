5443b093a3c30d0b3dfcb55cedec8ae71c6b5f15
Akhilben10 <akhil.bendalam2@gmail.com>
2023-01-17 12:29:14 +0530

    happy hacking

--- frontend/lib/src/App.js
+++ frontend/lib/src/App.js
@@ -1,14 +1,32 @@
-
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import{ AppLayout } from "./components/layout/app-layout"
-
 function App() {
   return (
     <div className="App">
-    
+      <Router>
         <AppLayout/>
-    </div>
+      </Router>
+    </div> 
   );
 }
-
 export default App;
+
+// import React, { Suspense } from 'react';
+// import { Container } from '@mui/material';
+// import {BrowserRouter as Router } from 'react-router-dom';
+// // import { NotificationContainer } from 'react-notifications';
+// import { AppLayout } from './components/layout/app-layout';
+// // import { UserProvider } from './context/user-context';
+// const App = () => (
+//   // <UserProvider>
+// //  <Suspense fallback={null}>
+//   <Container className='page-container'>
+//     <Router>
+//       <AppLayout/>
+//       {/* <NotificationContainer/>  */}
+//     </Router>
+//   </Container>
+//   // </Suspense>
+//   // </UserProvider>
+// )
+// export default App
--- frontend/lib/src/components/layout/app-layout.js
+++ frontend/lib/src/components/layout/app-layout.js
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import { Container } from '@mui/material';
-
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-
+import { BooksList } from "../books-list/books-list";
+import { Route, Routes, Navigate, Link} from "react-router-dom"
+import { Box } from '@mui/system';
+import {LoginDialog} from "../login/login-dialog"
+import { useState } from 'react';
+ 
 export const AppLayout = () => {
-    return(
+
+  const [openLoginDialog, setOpenLoginDialog] = useState(false)
+  
+  const handleLoginSubmit =(username, password) => {
+    console.log(username, password)
+  }
+  const handleLoginClose = () => {
+    setOpenLoginDialog(false)
+  }
+
+  return(
         <>
         <AppBar position="static">
             <Container maxWidth="xl">
@@ -16,20 +30,40 @@ export const AppLayout = () => {
           <IconButton
             size="large"
             edge="start"
-            color="inherit"
+             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
           >
           </IconButton>
+          <Link to="/" style={{textDecoration:"none", flexGrow: 1}}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             LIBRARY
           </Typography>
-          <Button color="inherit">Login</Button>
+          </Link>
+          <Box
+            sx={{
+              flexGrow: 0,
+            }}
+            >
+          <Button
+           onClick={()=>{
+            setOpenLoginDialog(true)
+
+           }}
+           sx={{my:2, color: "white", display:"block"}} >Login</Button>
+          </Box>
         </Toolbar>
             </Container>
-
         </AppBar>
+        <Routes>
+          <Route path="/books" exact element={<BooksList/>}/>
+          <Route path="*" element={<Navigate to="/books"
+          replace/>}/> 
+        </Routes>
+        <LoginDialog open={openLoginDialog}
+        handlesubmit={handleLoginSubmit}
+        handleClose={handleLoginClose}
+        />  
         </>
     )
-}
-
+}    
